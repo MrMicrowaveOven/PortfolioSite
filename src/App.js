05f777b6ae6951dd3ec9a763201ac813d52ab31b
mrmicrowaveoven <benj@zagorski.com>
2022-04-26 09:32:41 -0700

    Spacing styling

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ const App = () => {
       <Section name="about me">
         <h3>Full-Stack Software Engineer</h3>
         <h4>Available for Hire</h4>
+        <br/>
         <p>I'm Benjamin, an energetic web developer with over 4 years experience in Ruby on Rails and React.  I come from a background in mathematics, education, and probability.</p>
+        <br/>
         <p>I love coding, so I have a lot of personal projects (see below).  I turn every project and assignment into an opportunity to learn.  I am a big fan of playing around with different libraries to learn the ins and outs of them.  I enjoy writing tests, building interfaces, and everything about favicons.</p>
+        <br/>
         <p>Outside of software, I love swimming in the summer and skiing in the winter.  Beach bonfires, dinner parties, and jacuzzis are my favorite things about existing.</p>
       </Section>
       <Section name="skills">
