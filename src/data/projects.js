4b44a85adcbc22d033eae61ef4ddd235cc9dbd8f
mrmicrowaveoven <benj@zagorski.com>
2022-04-26 12:03:57 -0700

    Added projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+export const projectData = [
+    {
+        displayName: "DrinkSelector",
+        tagline: "Try a new drink tonight!",
+        imgSource: "screenshots/DrinkSelector.png",
+        link: "https://mrmicrowaveoven.github.io/DrinkSelector/",
+        github: "https://github.com/MrMicrowaveOven/DrinkSelector",
+    },
+    {
+        displayName: "Trapped Game",
+        tagline: "A game for learning useful things",
+        imgSource: "screenshots/Trapped.gif",
+        link: "http://mrmicrowaveoven.github.io/TrappedGame/",
+        github: "https://github.com/mrmicrowaveoven/TrappedGame",
+    },
+    {
+        displayName: "Needle-X",
+        tagline: "A Needle Exchange Resource",
+        imgSource: "screenshots/NeedleX.png",
+        link: "https://needle-x.herokuapp.com",
+        github: "https://github.com/MrMicrowaveOven/NeedleX",
+    },
+    {
+        displayName: "Sudoku Hint",
+        tagline: "Let me solve your Sudoku puzzle!",
+        imgSource: "screenshots/SudokuSolver.gif",
+        link: "https://sudoku-hint.herokuapp.com/",
+        github: "https://github.com/MrMicrowaveOven/sudoku-solver",
+    },
+    {
+        displayName: "Psychic Marilyn",
+        tagline: "Marilyn tells when to leave for Bart",
+        imgSource: "screenshots/PsychicMarilyn.png",
+        link: "https://psychic-marilyn.herokuapp.com/",
+        github: "https://github.com/MrMicrowaveOven/PsychicMarilyn",
+    },
+    {
+        displayName: "DrawCircle",
+        tagline: "How to make a circle from a set of points",
+        imgSource: "screenshots/DrawCircle.gif",
+        link: "https://mrmicrowaveoven.github.io/DrawCircle/",
+        github: "https://github.com/MrMicrowaveOven/DrawCircle",
+    },
+]
