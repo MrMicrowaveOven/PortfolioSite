c6a145f9a6e0510fa515e8ae465c9ef7597e2e1a
mrmicrowaveoven <benj@zagorski.com>
2022-04-25 22:33:19 -0700

    New styling, added icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import './css/backgrounds.css';
+import './css/skills.css'
+import { skillData } from './data/skills';
 
-function App() {
+const App = () => {
+  const skillsSection = skillData.map((skill, index) =>
+    <div className="skill" key={index}>
+      <a href={skill.link} target="_blank">
+        <img className="skill-icon" src={require(`${skill.imgSource}`)}/>
+      </a>
+      <h3>{skill.displayName}</h3>
+    </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section home">
+        <h1 className="name-text">Benjamin Zagorski</h1>
+        <h2 className="job-title-text">Full-Stack Software Engineer</h2>
+        <a>Download my Resume</a>
+        <a>Check out my Github</a>
+      </div>
+      <Section name="about me">
+        <h3>Full-Stack Software Engineer</h3>
+        <h4>Available for Hire</h4>
+        <p>I'm Benjamin, an energetic web developer with over 4 years experience in Ruby on Rails and React.  I come from a background in mathematics, education, and probability.</p>
+        <p>I love coding, so I have a lot of personal projects (see below).  I turn every project and assignment into an opportunity to learn.  I am a big fan of playing around with different libraries to learn the ins and outs of them.  I enjoy writing tests, building interfaces, and everything about favicons.</p>
+        <p>Outside of software, I love swimming in the summer and skiing in the winter.  Beach bonfires, dinner parties, and jacuzzis are my favorite things about existing.</p>
+      </Section>
+      <Section name="skills">
+        {skillsSection}
+      </Section>
+      <Section name="portfolio">
+
+      </Section>
+      <Section name="contact">
+
+      </Section>
     </div>
   );
+}
+
+const Section = ({children, name}) => {
+  return (
+    <div className={"section " + name}>
+      <h2 className="title">{name.toUpperCase()}</h2>
+      <div className={"content " + name}>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data/skills.jsx
@@ -0,0 +1,62 @@
+export const skillData = [
+    {
+        displayName: "Ruby",
+        imgSource: "./data/icons/ruby.png",
+        link: "https://www.google.com/search?q=ruby",
+    },
+    {
+        displayName: "Rails",
+        imgSource: "./data/icons/rails.png",
+        link: "https://www.google.com/search?q=rails",
+    },
+    {
+        displayName: "RSpec",
+        imgSource: "./data/icons/rspec.png",
+        link: "https://www.google.com/search?q=rspec",
+    },
+    {
+        displayName: "Python",
+        imgSource: "./data/icons/python.png",
+        link: "https://www.google.com/search?q=python",
+    },
+    {
+        displayName: "PostGresQL",
+        imgSource: "./data/icons/postgresql.png",
+        link: "https://www.google.com/search?q=postgresql",
+    },
+    {
+        displayName: "GraphQL",
+        imgSource: "./data/icons/graphql.png",
+        link: "https://www.google.com/search?q=graphql",
+    },
+    {
+        displayName: "Agile",
+        imgSource: "./data/icons/agile.png",
+        link: "https://www.google.com/search?q=agile",
+    },
+    {
+        displayName: "Javascript",
+        imgSource: "./data/icons/javascript.png",
+        link: "https://www.google.com/search?q=javascript",
+    },
+    {
+        displayName: "React",
+        imgSource: "./data/icons/react.svg",
+        link: "https://www.google.com/search?q=react",
+    },
+    {
+        displayName: "Redux",
+        imgSource: "./data/icons/redux.png",
+        link: "https://www.google.com/search?q=redux",
+    },
+    {
+        displayName: "HTML5",
+        imgSource: "./data/icons/html.png",
+        link: "https://www.google.com/search?q=html5",
+    },
+    {
+        displayName: "Git",
+        imgSource: "./data/icons/git.png",
+        link: "https://www.google.com/search?q=git",
+    },
+]
