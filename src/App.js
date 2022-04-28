760bf169797d5ba65332ea305a1e3fba8e04dcef
mrmicrowaveoven <benj@zagorski.com>
2022-04-27 20:15:29 -0500

    Added navMenu

--- src/App.js
+++ src/App.js
@@ -1,36 +1,92 @@
+import { useRef } from "react"
 import './css/App.css';
 import './css/backgrounds.css';
 import { Skills } from './Skills'
 import { Projects } from './Projects'
 
+
 const App = () => {
+  const homeElement = useRef(null)
+  const aboutElement = useRef(null)
+  const skillsElement = useRef(null)
+  const portfolioElement = useRef(null)
+  const contactElement = useRef(null)
+
+  const navigate = section => {
+    let topOfElement
+    switch (section) {
+      case "home":
+        topOfElement = homeElement.current.getBoundingClientRect().top
+        break
+      case "about":
+        topOfElement = aboutElement.current.getBoundingClientRect().top
+        console.log(topOfElement)
+        break
+      case "skills":
+        topOfElement = skillsElement.current.getBoundingClientRect().top
+        console.log(topOfElement)
+        break
+      case "portfolio":
+        topOfElement = portfolioElement.current.getBoundingClientRect().top
+        console.log(topOfElement)
+        break
+      case "contact":
+        topOfElement = contactElement.current.getBoundingClientRect().top
+        console.log(topOfElement)
+      break
+    }
+    window.scroll({top: topOfElement, behavior: "smooth"})
+  }
+
+  const navMenu = () => {
+    return (
+      <div className="nav-menu">
+        {["home", "about", "skills", "portfolio", "contact"].map((sectionName, index) =>
+          <div className="nav-menu-block" key={index} onClick={() => navigate(sectionName)}>
+            <div className="nav-menu-text">
+              {sectionName.toUpperCase()}
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
   return (
     <div className="App">
+      {navMenu()}
       <div className="section home">
         <h1 className="name-text">Benjamin Zagorski</h1>
         <h2 className="job-title-text">Full-Stack Software Engineer</h2>
         <a>Download my Resume</a>
         <a>Check out my Github</a>
       </div>
-      <Section name="about me">
-        <h3>Full-Stack Software Engineer</h3>
-        <h4>Available for Hire</h4>
-        <br/>
-        <p>I'm Benjamin, an energetic web developer with over 4 years experience in Ruby on Rails and React.  I come from a background in mathematics, education, and probability.</p>
-        <br/>
-        <p>I love coding, so I have a lot of personal projects (see below).  I turn every project and assignment into an opportunity to learn.  I am a big fan of playing around with different libraries to learn the ins and outs of them.  I enjoy writing tests, building interfaces, and everything about favicons.</p>
-        <br/>
-        <p>Outside of software, I love swimming in the summer and skiing in the winter.  Beach bonfires, dinner parties, and jacuzzis are my favorite things about existing.</p>
-      </Section>
-      <Section name="skills">
-        <Skills/>
-      </Section>
-      <Section name="portfolio">
-        <Projects />
-      </Section>
-      <Section name="contact">
+      <div ref={aboutElement}>
+        <Section name="about me">
+          <h3>Full-Stack Software Engineer</h3>
+          <h4>Available for Hire</h4>
+          <br/>
+          <p>I'm Benjamin, an energetic web developer with over 4 years experience in Ruby on Rails and React.  I come from a background in mathematics, education, and probability.</p>
+          <br/>
+          <p>I love coding, so I have a lot of personal projects (see below).  I turn every project and assignment into an opportunity to learn.  I am a big fan of playing around with different libraries to learn the ins and outs of them.  I enjoy writing tests, building interfaces, and everything about favicons.</p>
+          <br/>
+          <p>Outside of software, I love swimming in the summer and skiing in the winter.  Beach bonfires, dinner parties, and jacuzzis are my favorite things about existing.</p>
+        </Section>
+      </div>
+      <div ref={skillsElement}>
+        <Section name="skills">
+          <Skills/>
+        </Section>
+      </div>
+      <div ref={portfolioElement}>
+        <Section name="portfolio">
+          <Projects />
+        </Section>
+      </div>
+      <div ref={contactElement}>
+        <Section name="contact">
 
-      </Section>
+        </Section>
+      </div>
     </div>
   );
 }
