b9cd54940c2ae64bee65508772661ae8a261487b
mrmicrowaveoven <benj@zagorski.com>
2022-05-17 23:28:42 -0500

    Added github links

--- src/Projects.js
+++ src/Projects.js
@@ -1,15 +1,21 @@
 import { projectData } from "./data/projects"
+import GithubLogo from "./data/icons/github_icon.svg"
 
 export const Projects = () => {
     return (
         <div className="projects">
-            {projectData.map((project, index) => 
+            {projectData.map((project, index) =>
                 <div className="project" key={index}>
                     <a href={project.link} target="_blank">
                         <img className="project-image" src={require(`${project.imgSource}`)}></img>
                     </a>
                     <div className="project-name">{project.displayName}</div>
                     <p className="project-tagline">{project.tagline}</p>
+                    <div className="github-icon-container">
+                        <a href={project.github} target="_blank">
+                            <img className="github-icon" src={GithubLogo}></img>
+                        </a>
+                    </div>
                 </div>
             )}
         </div>
