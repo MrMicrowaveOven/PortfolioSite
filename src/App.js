c77845b7599d4db2b48691ea82c41ad8ac3f9933
mrmicrowaveoven <benj@zagorski.com>
2022-05-26 08:28:36 -0500

    Refactored NavMenu into separate component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './css/navMenu.css'
 import './css/project.css'
 import './css/skills.css'
 import './css/zMobile.css'
+import { NavMenu } from './NavMenu'
 import { Home } from './Home'
 import { About } from './About'
 import { Skills } from './Skills'
@@ -15,43 +16,9 @@ import { Projects } from './Projects'
 import { Contact } from './Contact'
 
 const App = () => {
-  const navigate = section => {
-    const elementId = section + '-element'
-    const element = document.getElementById(elementId)
-    const offSet = 41
-    const bodyRect = document.body.getBoundingClientRect().top
-    const elementRect = element.getBoundingClientRect().top
-    const elementPosition = elementRect - bodyRect
-    const offsetPosition = elementPosition - offSet
-
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: "smooth"
-    })
-  }
-
-  const navMenu = () => {
-    return (
-      <div className="nav-menu-wide">
-        <div className="nav-menu">
-          {["home", "about", "skills", "projects", "contact"].map((sectionName, index) =>
-            <div
-              className={`nav-menu-block ${index === 0 && "nav-menu-block-first"}`}
-              key={index}
-              onClick={() => navigate(sectionName)}
-            >
-              <div className="nav-menu-text">
-                {sectionName.toUpperCase()}
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    )
-  }
   return (
     <div className="App">
-      {navMenu()}
+      <NavMenu />
       <Section name="home">
         <Home />
       </Section>
---
+++ src/NavMenu.js
@@ -0,0 +1,33 @@
+export const NavMenu = () => {
+  const navigate = section => {
+    const elementId = section + '-element'
+    const element = document.getElementById(elementId)
+    const offSet = 41
+    const bodyRect = document.body.getBoundingClientRect().top
+    const elementRect = element.getBoundingClientRect().top
+    const elementPosition = elementRect - bodyRect
+    const offsetPosition = elementPosition - offSet
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+    })
+  }
+  return (
+    <div className="nav-menu-wide">
+      <div className="nav-menu">
+        {["home", "about", "skills", "projects", "contact"].map((sectionName, index) =>
+          <div
+            className={`nav-menu-block ${index === 0 && "nav-menu-block-first"}`}
+            key={index}
+            onClick={() => navigate(sectionName)}
+          >
+            <div className="nav-menu-text">
+              {sectionName.toUpperCase()}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
