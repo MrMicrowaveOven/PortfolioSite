b75e4ffd31aea861aea43abb3cae9470510cdadf
mrmicrowaveoven <benj@zagorski.com>
2022-05-26 07:33:35 -0500

    Emails now send

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 import LinkedInLogo from './data/icons/linkedin.png'
 import GithubLogo from './data/icons/github_icon.svg'
 
 export const Contact = () => {
+  const [emailInfo, setEmailInfo] = useState({
+    from_name: '',
+    to_name: 'mrmicrowaveoven@gmail.com',
+    message: '',
+    reply_to: '',
+  });
+  const handleChange = (e) => {
+    setEmailInfo({ ...emailInfo, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_toa61pc',
+      'template_u52gh0p',
+      emailInfo,
+      'ejI_EMnnF_RXl6Jyi'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
   return (
     <div className="contact-body">
       <div className="contact-info">
@@ -20,9 +47,30 @@ export const Contact = () => {
       </div>
       <div className="contact-form">
         <div className="contact-form-title">Say Hello!</div>
-        <div>Name</div>
-        <div>Email</div>
-        <div>Message</div>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            name='from_name'
+            placeholder='from name'
+            value={emailInfo.from_name}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            name='message'
+            placeholder='Your message'
+            value={emailInfo.message}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            name='reply_to'
+            placeholder='Your email'
+            value={emailInfo.reply_to}
+            onChange={handleChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     </div>
   )
