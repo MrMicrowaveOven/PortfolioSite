9bf3e5c0a40db0a8564ed505e6885e3823dc0150
mrmicrowaveoven <benj@zagorski.com>
2022-06-06 12:15:59 -0500

    Updated skill links, added React Native

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,62 +1,62 @@
 export const skillData = [
     {
-        displayName: "Ruby",
-        imgSource: "./data/icons/ruby.png",
-        link: "https://www.google.com/search?q=ruby",
-    },
-    {
-        displayName: "Rails",
-        imgSource: "./data/icons/rails.png",
-        link: "https://www.google.com/search?q=rails",
-    },
-    {
-        displayName: "RSpec",
-        imgSource: "./data/icons/rspec.png",
-        link: "https://www.google.com/search?q=rspec",
-    },
-    {
-        displayName: "Python",
-        imgSource: "./data/icons/python.png",
-        link: "https://www.google.com/search?q=python",
-    },
-    {
-        displayName: "PostGres",
-        imgSource: "./data/icons/postgresql.png",
-        link: "https://www.google.com/search?q=postgresql",
-    },
-    {
-        displayName: "GraphQL",
-        imgSource: "./data/icons/graphql.png",
-        link: "https://www.google.com/search?q=graphql",
-    },
-    {
-        displayName: "Agile",
-        imgSource: "./data/icons/agile.png",
-        link: "https://www.google.com/search?q=agile",
-    },
-    {
-        displayName: "Javascript",
-        imgSource: "./data/icons/javascript.png",
-        link: "https://www.google.com/search?q=javascript",
-    },
-    {
         displayName: "React",
         imgSource: "./data/icons/react.svg",
-        link: "https://www.google.com/search?q=react",
+        link: "https://reactjs.org/",
     },
     {
         displayName: "Redux",
         imgSource: "./data/icons/redux.png",
-        link: "https://www.google.com/search?q=redux",
+        link: "https://redux.js.org/",
+    },
+    {
+        displayName: "GraphQL",
+        imgSource: "./data/icons/graphql.png",
+        link: "https://graphql.org/",
+    },
+    {
+      displayName: "React Native",
+      imgSource: "./data/icons/react_native.png",
+      link: "https://reactnative.dev/",
+    },
+    {
+        displayName: "Agile",
+        imgSource: "./data/icons/agile.png",
+        link: "https://agilemanifesto.org/",
     },
     {
         displayName: "HTML5",
         imgSource: "./data/icons/html.png",
-        link: "https://www.google.com/search?q=html5",
+        link: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
     },
     {
         displayName: "Git",
         imgSource: "./data/icons/git.png",
-        link: "https://www.google.com/search?q=git",
+        link: "https://git-scm.com/",
+    },
+    {
+        displayName: "Ruby",
+        imgSource: "./data/icons/ruby.png",
+        link: "https://www.ruby-lang.org/",
+    },
+    {
+        displayName: "Rails",
+        imgSource: "./data/icons/rails.png",
+        link: "https://rubyonrails.org/",
+    },
+    {
+        displayName: "RSpec",
+        imgSource: "./data/icons/rspec.png",
+        link: "https://rspec.info/",
+    },
+    {
+        displayName: "Python",
+        imgSource: "./data/icons/python.png",
+        link: "https://www.python.org/",
+    },
+    {
+        displayName: "PostGres",
+        imgSource: "./data/icons/postgresql.png",
+        link: "https://www.postgresql.org/",
     },
 ]
