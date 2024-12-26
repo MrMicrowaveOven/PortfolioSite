4fcf213a712ff904418f7f2dc65690d0a614e044
mrmicrowaveoven <benj@zagorski.com>
2024-12-26 02:55:26 -0800

    Added algorithm data

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,9 +1,25 @@
+import { algorithmData } from "./data/algorithms"
+
 export const Algorithms = () => {
     return (
         <div>
             <p className="about-text">
                 These are just some fun things I wrote in passing.  Generally it's when there's a riddle I can't solve, or some statistical anomaly I don't understand.
             </p>
+            <div className="algorithms">
+                {algorithmData.map((algorithm) => {
+                    return (
+                        <div className="algorithm">
+                            <a href={algorithm.link} target="_blank">
+                                <div className="algorithm-box">
+                                    <div className="algorithm-name">{algorithm.displayName}</div>
+                                    <div className="algorithm-description">{algorithm.description}</div>
+                                </div>
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/data/algorithms.js
@@ -0,0 +1,18 @@
+export const algorithmData = [
+    {
+        displayName: "Prisoner Box Riddle",
+        link: "https://github.com/MrMicrowaveOven/prisoner-box-riddle",
+        description: "A simulation of the solution to the Prisoner Box Riddle",
+    },
+    {
+        displayName: "Alien Communique",
+        link: "https://github.com/MrMicrowaveOven/AlienCommunique",
+        description: "Our solution to the Alien Communique problem, Spring 2016",
+    },
+    {
+        displayName: "Clock Hands Riddle",
+        link: "https://github.com/MrMicrowaveOven/clock_hands_riddle",
+        description: "A clock simulation to solve the riddle: When do the hands of a clock cross each other?",
+    },
+
+]
